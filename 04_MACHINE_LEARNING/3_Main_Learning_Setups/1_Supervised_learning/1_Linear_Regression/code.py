ee92ee51ac6a13d3be31f89f1a5d065f4beff582
Abdulhakeem Muhammed <dev.abdulhakeem@gmail.com>
2026-05-14 09:08:09 +0100

    Add Classical Machine Learning Methods to Chapter 4

--- 04_MACHINE_LEARNING/3_Main_Learning_Setups/1_Supervised_learning/1_Linear_Regression/code.py
+++ 04_MACHINE_LEARNING/3_Main_Learning_Setups/1_Supervised_learning/1_Linear_Regression/code.py
@@ -10,18 +10,36 @@ We'll use the **California housing dataset** (a classic for regression) loaded v
 
 # IMPORT MODULES
 
-# data manupulation 
+# data manipulation
 import pandas as pd
 import numpy as np
 # visualization
 import matplotlib.pyplot as plt
 import seaborn as sns
+
 # preprocessing and modeling
-from sklearn.model_selection import train_test_split, cross_val_score, KFold, learning_curve
+from sklearn.model_selection import cross_val_score, train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.preprocessing import StandardScaler
-# preloaded dataset
+
+# dataset
 from sklearn.datasets import fetch_california_housing
 
 
+# load dataset
+df = fetch_california_housing()
+
+# view dataset
+# df
+# print(df.data)
+# print(df.describe)
+
+# check missing data
+df.isnull().sum()
+df.isnull().sum().sum()
+
+# check duplicates
+df.duplicate().sum()
+
+# check dataframe columns
